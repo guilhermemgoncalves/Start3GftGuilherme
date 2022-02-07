b73f454a308064c9606c7c8d945ed94930769351
guilhermemgoncalves <guilherme_marciano@outlook.com.br>
2022-02-07 18:15:12 -0300

    Adição do Ex4!

---
+++ Ex4TrabalhandoComArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex4TrabalhandoComArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TArrays array = new TArrays();
+            int[] arrayT = { 10, 15, 20, -30, 15 }; // dados mocados para o teste da main
+
+            array.recebeArray(arrayT);
+            Console.Write("Soma de todos os numeros: ");
+            Console.WriteLine(array.SomaTodos());
+            Console.Write("Maior numero: ");
+            Console.WriteLine(array.MaiorNumero());
+            Console.Write("Menor numero: ");
+            Console.WriteLine(array.MenorNumero());
+            Console.Write("Media dos valores: ");
+            Console.WriteLine(Convert.ToDouble(array.SomaTodos())/array.ContaTodos());
+            
+
+
+
+        }
+    }
+}
---
+++ Ex4TrabalhandoComArray/TArrays.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex4TrabalhandoComArray
+{
+    public class TArrays
+    {
+        int[] ArrayRec = new int[100];
+
+
+        public void recebeArray (int[] arrayrecebido)
+        {
+            this.ArrayRec = arrayrecebido;
+        }
+
+        
+
+        public int SomaTodos()
+            
+        {
+            int somatoria = 0;
+            foreach (var item in ArrayRec)
+            {
+                somatoria += item;
+            }
+            return somatoria;
+        }
+
+        public int MaiorNumero()
+        {
+            int maiorNumero = ArrayRec[0];
+            foreach(var item in ArrayRec)
+            {
+                if (item > maiorNumero)
+                    maiorNumero = item;
+
+            }
+            return maiorNumero;
+
+        } 
+        public int MenorNumero()
+        {
+            int menorNumero = ArrayRec[0];
+            foreach(var item in ArrayRec)
+            {
+                if (item < menorNumero)
+                    menorNumero = item;
+
+            }
+            return menorNumero;
+
+        }
+
+        public double ContaTodos()
+        {
+            int contaItens = 0;
+            foreach (var item in ArrayRec)
+            {
+                if (item !=0)
+                {
+                    contaItens++;
+                }
+                            }
+
+            return contaItens;
+        }
+
+       
+    }
+}
